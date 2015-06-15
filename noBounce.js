32d2e867987abcafd2788141365060bdea1ef8f3
Tim Bartsch <timbartsch.de@gmail.com>
2015-06-15 20:52:05 +0200

    Formatted js and example.

--- noBounce.js
+++ noBounce.js
@@ -1,28 +1,28 @@
-var noBounce = function(){
-	var module = {};
+var noBounce = function() {
+  var module = {};
 
   var settings = {
     animate: true
   };
 
-	var track = [];
+  var track = [];
 
-	var velocity = {
-		x: 0,
-		y: 0
-	};
+  var velocity = {
+    x: 0,
+    y: 0
+  };
 
   var vector = {
-    subtraction: function(v1, v2){
+    subtraction: function(v1, v2) {
       return {
         x: v1.x - v2.x,
         y: v1.y - v2.y
       };
     },
     length: function(v) {
-		  return Math.sqrt((v.x * v.x) + (v.y * v.y));
-	  },
-    unit: function(v){
+      return Math.sqrt((v.x * v.x) + (v.y * v.y));
+    },
+    unit: function(v) {
       var length = vector.length(v);
       v.x /= length;
       v.y /= length;
@@ -33,108 +33,108 @@ var noBounce = function(){
     }
   };
 
-  var requestAnimFrame = (function(){
-      return  window.requestAnimationFrame       || 
-              window.webkitRequestAnimationFrame || 
-              window.mozRequestAnimationFrame    || 
-              window.oRequestAnimationFrame      || 
-              window.msRequestAnimationFrame     || 
-              function( callback ){
-                window.setTimeout(callback, 1000 / 60);
-              };
-    })();
+  var requestAnimFrame = (function() {
+    return window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function(callback) {
+        window.setTimeout(callback, 1000 / 60);
+      };
+  })();
 
-	function handleTouchStart (evt){
-		var point,
-			touch;
+  function handleTouchStart(evt) {
+    var point,
+      touch;
 
-		touch = evt.changedTouches[0];
-		point = {
-			x: touch.clientX,
-			y: touch.clientY,
-			timeStamp: evt.timeStamp
-		};
-		track = [point];
-	}
+    touch = evt.changedTouches[0];
+    point = {
+      x: touch.clientX,
+      y: touch.clientY,
+      timeStamp: evt.timeStamp
+    };
+    track = [point];
+  }
 
-	function handleTouchMove (evt){	
-		var point,
-			touch;
+  function handleTouchMove(evt) {
+    var point,
+      touch;
 
     evt.preventDefault();
 
-		touch = evt.changedTouches[0];
-		point = {
-			x: touch.clientX,
-			y: touch.clientY,
-			timeStamp: evt.timeStamp
-		};
-		track.push(point);
+    touch = evt.changedTouches[0];
+    point = {
+      x: touch.clientX,
+      y: touch.clientY,
+      timeStamp: evt.timeStamp
+    };
+    track.push(point);
     doScroll();
-	}
+  }
 
-	function handleTouchEnd (evt){
-		if(track.length > 2 && settings.animate){
-			velocity = calcVelocity();
+  function handleTouchEnd(evt) {
+    if (track.length > 2 && settings.animate) {
+      velocity = calcVelocity();
       requestAnimFrame(animate);
-		}
-	}
+    }
+  }
 
-	function calcVelocity(){
-		var p1,
-        p2,
-        v,
-        timeDiff,
-        length;
-
-		p1 = track[0];
-		p2 = track[track.length-1];
-		timeDiff = p2.timeStamp - p1.timeStamp;
-		v = vector.subtraction(p2, p1);
-		length = vector.length(v);
-		vector.unit(v);
-		vector.skalarMult(v, length / timeDiff * 20);
-		return v;
-	}
-
-  function doScroll(){
+  function calcVelocity() {
     var p1,
-        p2,
-        x,
-        y;
+      p2,
+      v,
+      timeDiff,
+      length;
 
-    if(track.length > 1){
-      p1 = track[track.length -1];
-      p2 = track[track.length -2];
+    p1 = track[0];
+    p2 = track[track.length - 1];
+    timeDiff = p2.timeStamp - p1.timeStamp;
+    v = vector.subtraction(p2, p1);
+    length = vector.length(v);
+    vector.unit(v);
+    vector.skalarMult(v, length / timeDiff * 20);
+    return v;
+  }
+
+  function doScroll() {
+    var p1,
+      p2,
+      x,
+      y;
+
+    if (track.length > 1) {
+      p1 = track[track.length - 1];
+      p2 = track[track.length - 2];
       x = p2.x - p1.x;
       y = p2.y - p1.y;
-      requestAnimFrame(function(){
+      requestAnimFrame(function() {
         window.scrollBy(x, y);
       });
     }
   }
 
-  function animate(){
+  function animate() {
     scrollBy(-velocity.x, -velocity.y);
     vector.skalarMult(velocity, 0.95);
-    if(vector.length(velocity) > 0.2){
+    if (vector.length(velocity) > 0.2) {
       requestAnimFrame(animate);
     }
   }
 
-  //Returns true if it is a DOM element    
-function isElement(o){
-  return (
-    typeof HTMLElement === "object" ? o instanceof HTMLElement : //DOM2
-    o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName==="string"
-);
-}
+  //Returns true if it is a DOM element
+  function isElement(o) {
+    return (
+      typeof HTMLElement === "object" ? o instanceof HTMLElement : //DOM2
+      o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName === "string"
+    );
+  }
 
-  module.init = function(options){
-    if(typeof options.animate === "boolean"){
+  module.init = function(options) {
+    if (typeof options.animate === "boolean") {
       settings.animate = options.animate;
     }
-    if(isElement(options.element)) {
+    if (isElement(options.element)) {
       settings.element = options.element;
     }
 
@@ -147,6 +147,5 @@ function isElement(o){
     element.addEventListener("touchleave", handleTouchEnd);
   };
 
-	return module;
+  return module;
 }();
-
